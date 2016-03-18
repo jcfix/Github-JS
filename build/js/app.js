122126a2eb97c8bf07c68b45fd4014de416e3950
Jessica Fix <jcfix.pdx@gmail.com>
2016-03-18 08:50:58 -0700

    Begin function to make API request and append to html div

--- build/js/app.js
+++ build/js/app.js
@@ -1,31 +1,14 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-exports.apiKey = "f3e6a48c80583843f72aecc17212d22a01af5031";
-
-},{}],2:[function(require,module,exports){
-// exports.Github = function(argument){
-//   this.argument = argument;
-// };
+// var Constructor = require('../js/function-name.js').Constructor;
 //
-// exports.Github.prototype.method = function() {
+// $(document).ready(function() {
 //
-// };
-
-exports.getRepos = function() {
-  $.get('')
-}
-
-},{}],3:[function(require,module,exports){
-var apiKey = require('./../.env').apiKey;
-var Constructor = require('../js/function-name.js').Constructor;
-
-$(document).ready(function() {
-
-var newConstructor = new Constructor();
-
-
-});
+// var newConstructor = new Constructor();
+//
+//
+// });
 
 
 /* Front-end User interface (jquery) code goes in this file */
 
-},{"../js/function-name.js":2,"./../.env":1}]},{},[3]);
+},{}]},{},[1]);
---
+++ js/get-repositories.js
@@ -0,0 +1,19 @@
+var apiKey = require('./../.env').apiKey;
+
+// exports.Github = function(argument){
+//   this.argument = argument;
+// };
+//
+// exports.Github.prototype.method = function() {
+//
+// };
+
+exports.getRepos = function() {
+  var username = $('#ghUsername').val();
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
+    console.log(response);
+    $('div.search-result').html('<h1>' + username + '</h1>');
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+};
